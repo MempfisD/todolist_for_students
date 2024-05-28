8378b8f5516b907780f6c0933e446ceba73bbcd5
Dmitry Naumov <komrad.nda@yandex.ru>
2024-05-28 22:38:24 +0300

    1 todolist for students 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
-import {Todolist} from './Todolist';
+import {TaskType, Todolist} from './Todolist';
+
+let tasks1: Array<TaskType> = [
+	{id: 1, title: 'HTML & CSS', isDone: true},
+	{id: 1, title: 'JS', isDone: true},
+	{id: 1, title: 'React', isDone: false}
+]
+
+let tasks2: Array<TaskType> = [
+	{id: 1, title: 'Terminator', isDone: true},
+	{id: 1, title: 'XXX', isDone: true},
+	{id: 1, title: 'Gladiator', isDone: true}
+]
+
 
 function App() {
 	return (
 			<div className="App">
-				<Todolist/>
-				<Todolist/>
-				<Todolist/>
+				<Todolist title={'What to learn'} tasks={tasks1}/>
+				<Todolist title={'Movies'} tasks={tasks2}/>
 			</div>
 	);
 }
